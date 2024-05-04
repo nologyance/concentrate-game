00005a7f23cd0c26e5f8be8acbe2a57b01cf9e26
nologyance <nologyance>
2024-05-04 20:54:24 +0900

    init

---
+++ app/routes/games._index.tsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  return (
+    <div className="flex space-between">
+      <div className="h-24 w-24 border-solid border-2">
+        <p>51</p>
+      </div>
+      <div className="h-24 w-24 border-solid border-2">
+        <p>85</p>
+      </div>
+    </div>
+  )
+}
