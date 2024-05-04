80a9def306b11fdcd7890b1c1f0ac8ccc9a01c00
nologyance <nologyance>
2024-05-05 00:16:30 +0900

    数字が同じだったらOK

--- app/routes/games._index.tsx
+++ app/routes/games._index.tsx
@@ -1,12 +1,50 @@
+import { useState } from "react"
+
 export default function Index() {
+  const [answer, setAnswer] = useState({ left: "", right: "" })
+  const handleClick = (e: any) => {
+    const currentAnswer = e.target.innerHTML
+    if (answer.left === "") {
+      setAnswer({ left: currentAnswer, right: "" })
+      return
+    }
+    setAnswer({ ...answer, right: currentAnswer })
+
+    if (answer.left === currentAnswer) {
+      console.log("終了！")
+    } else {
+      console.log("続行！")
+    }
+  }
   return (
-    <div className="flex space-between">
-      <div className="h-24 w-24 border-solid border-2">
-        <p>51</p>
+    <>
+      <h2> 説明 </h2>
+      <div className="flex space-between bg-slate-100">
+        <div
+          className="h-24 w-24 border-solid border-2 bg-white"
+          onClick={handleClick}
+        >
+          51
+        </div>
+        <div
+          className="h-24 w-24 border-solid border-2 bg-white"
+          onClick={handleClick}
+        >
+          85
+        </div>
       </div>
-      <div className="h-24 w-24 border-solid border-2">
-        <p>85</p>
+      <div className="flex">
+        {answer.left && (
+          <>
+            <div className="h-24 w-24 border-solid border-2 bg-white">
+              <p>{answer.left}</p>
+            </div>
+            <div className="h-24 w-24 border-solid border-2 bg-white">
+              <p>{answer.right}</p>
+            </div>
+          </>
+        )}
       </div>
-    </div>
+    </>
   )
 }
